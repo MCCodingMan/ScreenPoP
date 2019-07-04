c97202627d134bcc8d5c23976fb21a3a4d846ac9
MCCodingMan <250906711@qq.com>
2019-07-04 15:46:49 +0800

    framewor

---
+++ Framework/ScreenPoP.framework/Headers/ScreenPoP.h
@@ -0,0 +1,21 @@
+//
+//  ScreenPoP.h
+//  ScreenPoP
+//
+//  Created by wr on 2019/7/3.
+//  Copyright © 2019年 wanmengchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScreenPoP.
+FOUNDATION_EXPORT double ScreenPoPVersionNumber;
+
+//! Project version string for ScreenPoP.
+FOUNDATION_EXPORT const unsigned char ScreenPoPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScreenPoP/PublicHeader.h>
+
+#import <ScreenPoP/PopGestureRecognizerManager.h>
+#import <ScreenPoP/PopGestureRecognizerManagerConfiger.h>
+
